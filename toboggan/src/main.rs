917e85696a4672cf3d5c754711159ab4651cfa09
karthikeyan-yegappan <karthikeyan.yegappan@bbc.co.uk>
2020-12-08 16:27:38 +0000

    open file and read contents

--- toboggan/src/main.rs
+++ toboggan/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // --snip--
+    println!("In file {}", "day2-input.txt");
+
+    let contents = fs::read_to_string("day2-input.txt")
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
