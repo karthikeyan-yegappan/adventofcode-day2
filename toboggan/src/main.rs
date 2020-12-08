4e9bd490bf99891a59fd8bb4038de39919c33b8b
karthikeyan-yegappan <karthikeyan.yegappan@bbc.co.uk>
2020-12-08 20:45:59 +0000

    prep for second puzzle

--- toboggan/src/main.rs
+++ toboggan/src/main.rs
@@ -5,28 +5,41 @@ use std::convert::TryInto;
 fn main() {
     // --snip--
     println!("In file {}", "day2-input.txt");
+
+}
+
+fn _puzzle1_password() {
     let reader = BufReader::new(File::open("day2-input.txt").expect("Cannot open file.txt"));
     let mut _count = 0;
     let mut _total_passwords = 0;
-
     for line in reader.lines() {
-       let words = String::from(line.unwrap());
-       let v: Vec<&str> = words.split(' ').collect();
-       let _low_high = v[0];
-       let mut _password_letter = String::from(v[1]);
-       let mut _password = v[2];
-       let numbers: Vec<&str> = _low_high.split('-').collect();
-       let _low: i32 = numbers[0].parse().unwrap_or(0);
-       let _high: i32 = numbers[1].parse().unwrap_or(0);
-       let _letter_counts: i32 = _password.matches(_password_letter.remove(0)).count().try_into().unwrap();
-      
-       if _letter_counts > _high || _letter_counts < _low || _letter_counts == 0 {
-            _count = _count + 1;
-        }
+        let words = String::from(line.unwrap());
+        let v: Vec<&str> = words.split(' ').collect();
+        let _low_high = v[0];
+        let mut _password_letter = String::from(v[1]);
+        let mut _password = v[2];
+        let numbers: Vec<&str> = _low_high.split('-').collect();
+        let _low: i32 = numbers[0].parse().unwrap_or(0);
+        let _high: i32 = numbers[1].parse().unwrap_or(0);
+        let _letter_counts: i32 = _password.matches(_password_letter.remove(0)).count().try_into().unwrap();
+       
+        if _letter_counts > _high || _letter_counts < _low || _letter_counts == 0 {
+             _count = _count + 1;
+         }
+ 
+         _total_passwords += 1;
+     }
+     println!("Wrong passwords: {}",_count);
+     println!("Total passwords: {}",_total_passwords);
+     println!("Correct passwords: {}",_total_passwords - _count);
+}
 
-        _total_passwords += 1;
-    }
+fn _puzzle2_password() {
+    let _reader = BufReader::new(File::open("day2-input.txt").expect("Cannot open file.txt"));
+    let mut _count = 0;
+    let mut _total_passwords = 0;
 
+    
     println!("Wrong passwords: {}",_count);
     println!("Total passwords: {}",_total_passwords);
     println!("Correct passwords: {}",_total_passwords - _count);
