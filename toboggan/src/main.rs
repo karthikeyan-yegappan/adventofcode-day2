e7e3cfa4c210689802903e67acfe907bff37267f
karthikeyan-yegappan <karthikeyan.yegappan@bbc.co.uk>
2020-12-08 17:15:34 +0000

    split lines to vector

--- toboggan/src/main.rs
+++ toboggan/src/main.rs
@@ -5,14 +5,11 @@ fn main() {
     // --snip--
     println!("In file {}", "day2-input.txt");
     let reader = BufReader::new(File::open("day2-input.txt").expect("Cannot open file.txt"));
-
-    // println!("With text:\n{}", contents);
-
     for line in reader.lines() {
-        for word in line.unwrap().split_whitespace() {
-            println!("word '{}'", word);
-        }
-        println!("end of each line");
+       let words = String::from(line.unwrap());
+       let v: Vec<&str> = words.split(' ').collect();
+       println!("{:?}", v);
+        
     }
-
+   // Ok(());
 }
